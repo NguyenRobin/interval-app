fbcc3a3e58124f74a09837b1d8c5dc7bf69d68d0
NguyenRobin <robin.nnguyen@hotmail.com>
2023-10-11 23:06:52 +0200

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-actions',
-  // server: {
-  //   watch: {
-  //     usePolling: true,
-  //   },
-  //   host: true, // needed for the Docker Container port mapping to work
-  //   strictPort: true,
-  //   port: 5173, // you can replace this port with any port
-  // },
+  base: '/interval-app',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  },
 });
