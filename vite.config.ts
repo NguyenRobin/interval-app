687a374488685eb5b0aa163815e396ac450ec822
NguyenRobin <robin.nnguyen@hotmail.com>
2023-10-11 23:08:51 +0200

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/interval-app',
+  base: '/interval-app/',
   server: {
     watch: {
       usePolling: true,
